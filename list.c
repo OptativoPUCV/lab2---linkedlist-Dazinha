c55d5eda300d8613eec21d301c1ced4573737757
Replit user <>
2023-08-24 20:50:13 +0000

    new try

--- list.c
+++ list.c
@@ -144,6 +144,7 @@ void pushBack(List * list, void * data)
 void pushCurrent(List * list, void * data)
 {
   Node* newNode = createNode(data);
+  newNode-> prev = list-> head;
   
   list-> current -> next = newNode;
   list-> tail = newNode;
